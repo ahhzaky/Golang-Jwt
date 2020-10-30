6531f5be35da7e52499182db79906cb57b3124ff
ahhzaky <siahmadzaky@gmail.com>
2020-10-30 11:24:46 +0700

    :gear: in process

--- client/go.mod
+++ client/go.mod
@@ -1,3 +1,5 @@
 module github.com/ahhzaky/Golang-Jwt/client
 
 go 1.15
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
--- client/main.go
+++ client/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+var mySigningKey = []byte("mysupersecretphrase")
+
+func GeneateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["user"] = "Elliot Forbes"
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+
+	tokenString, err := token.SignedString(mySigningKey)
+
+	if err != nil {
+		fmt.Errorf("Something went wrong: %s", err.Error())
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func main() {
+	fmt.Println("simple rest api")
+
+	tokenString, err := GeneateJWT()
+	if err != nil {
+		fmt.Println("Something went wrong")
+	}
+
+	fmt.Println(tokenString)
+}
+
+// https://www.youtube.com/watch?v=-Scg9INymBs  (804)
